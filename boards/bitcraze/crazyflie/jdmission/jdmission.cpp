a228156d7543adb30e6d4f18466769056088d4c4
iskim <iskim@junilab.co.kr>
2021-10-18 00:01:05 +0900

    sensor signal

---
+++ boards/bitcraze/crazyflie/jdmission/jdmission.cpp
@@ -0,0 +1,181 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2016 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file syslink_main.cpp
+ * Entry point for syslink module used to communicate with the NRF module on a Crazyflie
+ */
+
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/tasks.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/defines.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <poll.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <mqueue.h>
+
+#include <drivers/drv_board_led.h>
+
+#include <systemlib/err.h>
+
+#include <board_config.h>
+
+#include "jdmission.h"
+
+
+JDMission::JDMission():
+	ModuleParams(nullptr),
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::hp_default)
+{
+	packet.goldenkey = 0x6114A826;
+	packet.length = 30;
+	packet.option = 2;
+}
+
+
+JDMission::~JDMission()
+{
+}
+
+
+void JDMission::Checksum(packet_t *pkt)
+{
+	int sum=pkt->option&0xFF;
+
+	sum += (pkt->option>>8)&0xFF;
+	for(int n=0; n<pkt->length-8; n++)
+		sum += pkt->data8[n];
+	pkt->checksum = (sum&0xFF);
+}
+
+void JDMission::Run()		//2ms
+{
+	hrt_abstime now_us = hrt_absolute_time();
+
+	if(_sensor_gyro_sub.updated()){
+		sensor_gyro_s gyro;
+		if (_sensor_gyro_sub.update(&gyro)) {
+			packet.data16[0] = (int16_t)(gyro.x*20);
+			packet.data16[1] = (int16_t)(gyro.y*20);
+			packet.data16[2] = (int16_t)(gyro.z*20);
+		}
+	}
+	if(_sensor_accel_sub.updated()){
+		sensor_accel_s accel;
+		if (_sensor_accel_sub.update(&accel)) {
+			packet.data16[3] = (int16_t)(accel.x*10);
+			packet.data16[4] = (int16_t)(accel.y*10);
+			packet.data16[5] = (int16_t)(accel.z*10);
+		}
+	}
+	if(_optical_flow_sub.updated()){
+		optical_flow_s optical_flow;
+		if(_optical_flow_sub.update(&optical_flow)){
+			packet.data16[6] = (int16_t)(optical_flow.pixel_flow_x_integral*200);
+			packet.data16[7] = (int16_t)(optical_flow.pixel_flow_y_integral*200);
+		}
+	}
+
+	if(_distance_sensor_sub.updated()){
+		distance_sensor_s distance_sensor;
+		if(_distance_sensor_sub.update(&distance_sensor)){
+			packet.data16[8] = (int16_t)(distance_sensor.current_distance*100);
+		}
+	}
+	if((now_us-last_time)>20000){
+		Checksum(&packet);
+		write(_fd, (const void *)&packet, packet.length);
+
+		last_time = now_us;
+	}
+}
+
+bool JDMission::init()
+{
+	_fd = px4_open("/dev/ttyS2", O_RDWR | O_NOCTTY);
+	if(_fd < 0)
+		return false;
+
+	if (!_sensor_gyro_sub.registerCallback())
+		return false;
+
+	ScheduleNow();
+	return true;
+}
+
+int JDMission::task_spawn(int argc, char *argv[])
+{
+	JDMission *instance = new JDMission();
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init()) {
+			return PX4_OK;
+		}
+
+	} else {
+		PX4_ERR("alloc failed");
+	}
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+	return PX4_ERROR;
+}
+
+int JDMission::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int JDMission::print_usage(const char *reason)
+{
+//	printf("fd:%d\r\n", _fd);
+	return 0;
+}
+
+extern "C" __EXPORT int jdmission_main(int argc, char *argv[])
+{
+	return JDMission::main(argc, argv);
+}
---
+++ boards/bitcraze/crazyflie/jdmission/jdmission.h
@@ -0,0 +1,67 @@
+
+#pragma once
+
+#include <stdint.h>
+
+#include <battery/battery.h>
+
+#include <drivers/device/device.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/defines.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/tasks.h>
+
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/PublicationMulti.hpp>
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/battery_status.h>
+#include <uORB/topics/input_rc.h>
+#include <uORB/topics/sensor_gyro.h>
+#include <uORB/topics/sensor_accel.h>
+#include <uORB/topics/distance_sensor.h>
+#include <uORB/topics/optical_flow.h>
+
+typedef struct __attribute__((__packed__)){
+	uint32_t goldenkey;
+	uint8_t length;
+	uint8_t checksum;
+	uint16_t option;
+	union{
+		int8_t data8[40];
+		int16_t data16[20];
+		int32_t data32[10];
+	};
+}packet_t;
+
+
+class JDMission : public ModuleBase<JDMission>, public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+	JDMission();
+	~JDMission() override;
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+	bool init();
+
+private:
+
+	uORB::SubscriptionCallbackWorkItem _sensor_gyro_sub{this, ORB_ID(sensor_gyro)};
+	uORB::Subscription _sensor_accel_sub{ORB_ID(sensor_accel)};
+	uORB::Subscription _distance_sensor_sub{ORB_ID(distance_sensor)};
+	uORB::Subscription _optical_flow_sub{ORB_ID(optical_flow)};
+	void Checksum(packet_t *pkt);
+	void task_main();
+	void Run() override;
+
+	int _fd;
+	packet_t packet;
+	hrt_abstime last_time;
+};
+
+
